e7a2480443fdc83e1fe9afc265b4ff9dfb1200a2
Guillaume Cabanel <guillaume.cabanel@gmail.com>
2016-11-14 17:13:42 +0100

    dynamic pages

--- config.rb
+++ config.rb
@@ -31,6 +31,12 @@ activate :autoprefixer
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
 #  :which_fake_page => "Rendering a fake page with a local variable" }
 
+members = %w(seb romain guillaume)
+
+members.each do |name|
+  proxy "/flats/#{name}.html", "/flats/show.html", :locals => { :owner => name }, :ignore => true
+end
+
 ###
 # Helpers
 ###
